239ef400cb006cb907f737bd426af33655075b8d
Gabriel Llinàs Sansó <gllinas@iesmacardona.cat>
2024-02-25 15:37:25 +0100

    led encén quan ir

--- cansat/arduino/protocol_comunicacio/morse/morse_enviar/morse_enviar.ino
+++ cansat/arduino/protocol_comunicacio/morse/morse_enviar/morse_enviar.ino
@@ -1,27 +1,23 @@
-const int PIN_LED_ESPERA = 2;  //Verd
-const int PIN_LED_ENVIAR = 8;  //Blau
-const int PIN_LED_REBRE = 5;   //Vermell
+const int PIN_LED_VERD = 2;  //Verd
+const int PIN_LED_BLAU = 8;  //Blau
+const int PIN_LED_VERMELL = 5;   //Vermell
 
 const int PIN_BRUNZIDOR = 11;  //Millor si és analògic o
 const int PIN_IR = 6;          ////Digital
 
-const char CARACTER_INICI_CONNEXIO = 'x';
-const int TEMPS_PUNT = 1000;
-const int TEMPS_RETXA = 3000;
-
-bool connexio_terra = false;
+const int TEMPS_ESPERA = 1000;
 
 void setup() {
   Serial.begin(9600);
-  pinMode(PIN_LED_ESPERA, OUTPUT);
-  pinMode(PIN_LED_REBRE, OUTPUT);
-  pinMode(PIN_LED_ENVIAR, OUTPUT);
+  pinMode(PIN_LED_BLAU, OUTPUT);
+  pinMode(PIN_LED_VERD, OUTPUT);
+  pinMode(PIN_LED_VERMELL, OUTPUT);
   pinMode(PIN_BRUNZIDOR, OUTPUT);
   pinMode(PIN_IR, INPUT);
 
-  digitalWrite(PIN_LED_ESPERA, LOW);
-  digitalWrite(PIN_LED_ENVIAR, LOW);
-  digitalWrite(PIN_LED_REBRE, LOW);
+  digitalWrite(PIN_LED_VERMELL, LOW);
+  digitalWrite(PIN_LED_BLAU, LOW);
+  digitalWrite(PIN_LED_VERD, LOW);
 }
 
 void loop() {
@@ -29,6 +25,13 @@ void loop() {
   
   int lectura_ir = digitalRead(PIN_IR);
   Serial.println(lectura_ir);
+  if(lectura_ir == 0){
+    digitalWrite(PIN_LED_VERMELL, HIGH);
+    tone(PIN_BRUNZIDOR, 400);
+  }else{
+    digitalWrite(PIN_LED_VERMELL, LOW);
+    noTone(PIN_BRUNZIDOR);
+  }
 
-  delay(1000);
+  delay(TEMPS_ESPERA);
 }
